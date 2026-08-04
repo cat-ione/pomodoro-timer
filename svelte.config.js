c9cb7c06a1861f0cfa4451fe72d90f047c5ae706
cat-ione <ionewang@catione.dev>
2026-08-03 23:53:27 -0400

    vercel compatibility

--- svelte.config.js
+++ svelte.config.js
@@ -2,16 +2,17 @@
 // so we use adapter-static with a fallback to index.html to put the site in SPA mode
 // See: https://svelte.dev/docs/kit/single-page-apps
 // See: https://v2.tauri.app/start/frontend/sveltekit/ for more info
-import adapter from "@sveltejs/adapter-static";
+import adapterStatic from '@sveltejs/adapter-static';
+import adapterVercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+
+const adapter = process.env.VERCEL ? adapterVercel() : adapterStatic({fallback: "index.html"});
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter({
-      fallback: "index.html",
-    }),
+    adapter,
   },
 };
 
